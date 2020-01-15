b743fa5b54f6a3b29d7e52fe2526695852842a69
jaechang <jaechang0721k@gmail.com>
2020-01-15 20:11:42 +0900

    NULL

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+    public float speed;
+    public float jumpPower;
+
+    private Rigidbody2D myRigibody2D;
+    private Transform myTransform;
+
+    private float h = 0.0f;
+    private float v = 0.0f;
+
+    private Vector3 dir;
+    private Vector2 dir2;
+
+    private bool isGrounded = false;
+    public Vector2 debugVelocity;
+    void Start()
+    {
+        myRigibody2D = this.GetComponent<Rigidbody2D>();
+        myTransform = this.GetComponent<Transform>();
+        
+    }
+    
+    void FixedUpdate()
+    {
+        GetKeyDwon();
+    }
+    
+    void Update()
+    {
+        MyDebug();
+    }
+
+    private void MyDebug()
+    {
+        debugVelocity = myRigibody2D.velocity;
+    }
+
+    private void Move()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        dir = Vector3.right * h;
+        dir = dir.normalized;
+        myTransform.Translate(dir * speed * Time.deltaTime);
+
+    }
+
+
+    private void GetKeyDwon()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Move();
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)|| Input.GetKey(KeyCode.UpArrow))
+        {
+            Action();
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.LeftAlt) || Input.GetKeyDown(KeyCode.RightAlt))
+        {
+            Jump();
+        }
+    }
+
+
+    private void Jump()
+    {
+        myRigibody2D.AddForce(Vector2.up * jumpPower);
+    }
+
+    private void Action()
+    {
+
+    }
+
+}
+
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    public static PlayerInput instance;
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
